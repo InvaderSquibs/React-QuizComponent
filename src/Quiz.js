db4de32ff08e8c71c2d089dc977c5db18ff0298b
Zach Walton <zachary-walton@pluralsight.com>
2018-07-23 08:56:54 -0600

    Confused about last part

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizEnd from './QuizEnd'
 import QuizQuestion from './QuizQuestion.js'
 
 let quizData = require('./quiz_data.json')
@@ -10,13 +11,17 @@ class Quiz extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <div className='QuizQuestion'>
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />
-                </div>
-            </div>
-        )
+      const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
+
+      return (
+        <div>
+          <div className='QuizQuestion'>
+
+            <QuizEnd isQuizEnd={isQuizEnd} />
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />
+          </div>
+        </div>
+      )
     }
 }
 
